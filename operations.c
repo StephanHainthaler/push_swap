1b03653d598d378e46c880bb3c1ed781a2375938
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2023-11-20 15:12:50 +0100

    operations.c was norminetted and compiled with the correct flags.

--- operations.c
+++ operations.c
@@ -31,7 +31,6 @@ void	ft_push(t_list **lst1, t_list **lst2)
 {
 	t_list	*first_node;
 	t_list	*new;
-	t_list	*to_delete;
 	int		temp;
 
 	if (ft_lstsize(*lst1) < 1)
@@ -56,107 +55,78 @@ void	ft_rotate(t_list **lst)
 	*lst = first_node->next;
 	last_node->next = first_node;
 	first_node->next = NULL;
-
-	// if (ft_lstsize(lst) < 2)
-	// 	return ;
-	// while (lst != NULL)
-	// {
-	// 	ft_swap(&lst);
-	// 	if (lst->next == NULL)
-	// 		break ;
-	// 	lst = lst->next;
-	// }
 }
 
 void	ft_reverse_rotate(t_list **lst)
 {
-	// t_list	*first_node;
-	// t_list	*last_node;
-	// int		temp;
-	// int		i;
-
-	// if (ft_lstsize(lst) < 2)
-	// 	return ;
-	// first_node = *lst;
-	// last_node = ft_lstlast(first_node);
-	// *lst = first_node->next;
-
-		t_list	*head;
-	t_list	*tail;
+	int	i;
 
 	if (ft_lstsize(*lst) < 2)
 		return ;
-	head = *lst;
-	tail = ft_lstlast(head);
-	while (head)
+	i = 0;
+	while (i < ft_lstsize(*lst) - 1)
 	{
-		if (head->next->next == NULL)
-		{
-			 head->next = NULL;
-			 break ;
-		}
-		head = head->next;
+		ft_rotate(lst);
+		i++;
 	}
-	tail->next = *lst;
-	*lst = tail;
-
 }
 
-// // cc operations.c Libft/ft_lstnew.c Libft/ft_putlst_fd.c Libft/ft_lstadd_front.c Libft/ft_putnbr_fd.c Libft/ft_putendl_fd.c Libft/ft_lstsize.c Libft/ft_lstadd_back.c Libft/ft_lstlast.c
+// cc operations.c Libft/ft_lstnew.c Libft/ft_putlst_fd.c 
+// Libft/ft_lstadd_front.c Libft/ft_putnbr_fd.c 
+// Libft/ft_putendl_fd.c Libft/ft_lstsize.c 
+// Libft/ft_lstadd_back.c Libft/ft_lstlast.c
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	t_list	*lst1 = NULL;
-	t_list	*lst2 = NULL;
-	t_list	*new1 = ft_lstnew(1);
-	t_list	*new2 = ft_lstnew(2);
-	t_list	*new3 = ft_lstnew(3);
-	t_list	*new4 = ft_lstnew(4);
-	t_list	*new5 = ft_lstnew(5);
-	t_list	*new6;
+// int main(void)
+// {
+// 	t_list	*lst1 = NULL;
+// 	t_list	*lst2 = NULL;
+// 	t_list	*new1 = ft_lstnew(1);
+// 	t_list	*new2 = ft_lstnew(2);
+// 	t_list	*new3 = ft_lstnew(3);
+// 	t_list	*new4 = ft_lstnew(4);
+// 	t_list	*new5 = ft_lstnew(5);
 
-	ft_lstadd_front(&lst1, new1);
-	ft_lstadd_front(&lst1, new2);
-	ft_lstadd_back(&lst1, new3);
-	ft_lstadd_back(&lst1, new4);
-	ft_lstadd_back(&lst1, new5);
+// 	ft_lstadd_front(&lst1, new1);
+// 	ft_lstadd_front(&lst1, new2);
+// 	ft_lstadd_back(&lst1, new3);
+// 	ft_lstadd_back(&lst1, new4);
+// 	ft_lstadd_back(&lst1, new5);
 
-	printf("The content of lst1:\n");
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
+// 	printf("The content of lst1:\n");
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
 
-	printf("Swapping the first two elements in lst1:\n");
-	ft_swap(&lst1);
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
+// 	printf("Swapping the first two elements in lst1:\n");
+// 	ft_swap(&lst1);
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
 
-	printf("Rotating lst1:\n");
-	ft_rotate(&lst1);
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
+// 	printf("Rotating lst1:\n");
+// 	ft_rotate(&lst1);
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
 
-	printf("Reverse-Rotating lst1:\n");
-	ft_reverse_rotate(&lst1);
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
+// 	printf("Reverse-Rotating lst1:\n");
+// 	ft_reverse_rotate(&lst1);
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
 
-	printf("Pushing the first element from lst1 to lst2:\n");
-	ft_push(&lst1, &lst2);
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
-	ft_putlst_fd(lst2, 1);
-	write(1, "\n", 1);
+// 	printf("Pushing the first element from lst1 to lst2:\n");
+// 	ft_push(&lst1, &lst2);
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
+// 	ft_putlst_fd(lst2, 1);
+// 	write(1, "\n", 1);
 
-	printf("Pushing the first element from lst2 to lst1:\n");
-	ft_push(&lst2, &lst1);
-	ft_putlst_fd(lst1, 1);
-	write(1, "\n", 1);
-	ft_putlst_fd(lst2, 1);
-	write(1, "\n", 1);
+// 	printf("Pushing the first element from lst2 to lst1:\n");
+// 	ft_push(&lst2, &lst1);
+// 	ft_putlst_fd(lst1, 1);
+// 	write(1, "\n", 1);
+// 	ft_putlst_fd(lst2, 1);
+// 	write(1, "\n", 1);
 
-	ft_lstclear(&lst1);
-
-
-}
+// 	ft_lstclear(&lst1);
+// 	return (0);
+// }
